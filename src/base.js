16c9a3b5056e74afa8781ad5dd149fbad4a99387
Subham Mani <manisubham09@gmail.com>
2023-09-04 20:57:27 +0530

    add header and main

--- src/base.js
+++ src/base.js
@@ -7,11 +7,47 @@ function createHeader(){
     restaurantName.textContent = 'Flavours Of India';
 
     header.appendChild(restaurantName);
+    header.appendChild(createNav());
 
     return header;
+}
+
+function createNav(){
+    const nav = document.createElement('nav');
+
+    nav.classList.add('nav');
+    
+    const homeBtn = document.createElement('button');
+    homeBtn.classList.add('nav-btn');
+    homeBtn.classList.add('home-btn');
+    homeBtn.textContent = 'Home';
+
+    const menuBtn = document.createElement('button');
+    menuBtn.classList.add('nav-btn');
+    menuBtn.classList.add('menu-btn');
+    menuBtn.textContent = 'Menu';
+
+    const contactBtn = document.createElement('button');
+    contactBtn.classList.add('nav-btn');
+    contactBtn.classList.add('contact-btn');
+    contactBtn.textContent = 'Contact';
+    
+    nav.appendChild(homeBtn);
+    nav.appendChild(menuBtn);
+    nav.appendChild(contactBtn);
+
+    return nav;
+}
+
+function creatMain(){
+    const main = document.createElement('main');
+    main.classList.add('main');
+    
+    return main;
 }
 
 export default function initialize(){
     const content = document.getElementById('content');
     content.appendChild(createHeader());
+    content.appendChild(creatMain());
 }
